142aeab3466ac238977da092d8bd4926dcc880fd
Joseph Claver <jclaver@woodridgesoftware.com>
2023-10-04 15:44:03 -0600

    Started writing Befunge interpreter

---
+++ src/utils/BefungeInterpreter.ts
@@ -0,0 +1,59 @@
+class BefungeInterpreter {
+    #width: number;
+    #height: number;
+    #initialProgram: string[][];
+
+    #x: number; #vX: number;
+    #y: number; #vY: number;
+    #stringMode: boolean;
+    #program: string[][];
+    #stack: number[];
+
+    constructor(program: string[][]) {
+        this.#height = program.length;
+        this.#width = program[0].length;
+        this.#stack = [];
+
+        // copying the given program into an initial state and current state
+        this.#initialProgram = [];
+        this.#program = [];
+        for (let i = 0; i < this.#height; i++) {
+            this.#initialProgram.push([...program[i]]);
+            this.#program.push([...program[i]]);
+        }
+
+        this.#x = 0;
+        this.#y = 0;
+        this.#vX = 1;
+        this.#vY = 0;
+        this.#stringMode = false;
+    }
+
+    #push(n: number) {
+        this.#stack.push(n);
+    }
+
+    #pop() {
+        if (this.#stack.length) {
+            return this.#stack.pop();
+        } else {
+            return 0;
+        }
+    }
+
+    getState() {
+        let res: string[][] = [];
+        for (let i = 0; i < this.#height; i++) {
+            res.push([...this.#program[i]]);
+        }
+        return res;
+    }
+
+    getStack() {
+        return [...this.#stack];
+    }
+
+    step() {
+
+    }
+}
