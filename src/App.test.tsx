5535670d00666c373572cd5349c61ce9c3f6dd73
Joseph Claver <jclaver@woodridgesoftware.com>
2023-10-05 10:07:42 -0600

    Wrapped a test in a describe block

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,8 +3,10 @@ import { render, screen } from '@testing-library/react';
 import "@testing-library/jest-dom";
 import App from './App';
 
-test('has the word "befunge" somewhere in the app by default', () => {
-    render(<App />);
-    const befungeElements = screen.getAllByText(/befunge/i);
-    expect(befungeElements.length).toBeGreaterThan(0);
+describe('the app', () => {
+    test('has the word "befunge" somewhere in it', () => {
+        render(<App />);
+        const befungeElements = screen.getAllByText(/befunge/i);
+        expect(befungeElements.length).toBeGreaterThan(0);
+    });
 });
