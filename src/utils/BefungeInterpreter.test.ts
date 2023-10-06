405832631e113dfb90294fc7dfcd5e2059522418
Joseph Claver <jclaver@woodridgesoftware.com>
2023-10-06 10:45:12 -0600

    Built the Befunge interpreter, along with a script for testing its behavior
    Made the Befunge interpreter functional
    
    Created tests for the befunge interpreter
    
    Added tests for user input commands
    
    Renamed some tests, added a couple more tests relating to get and put commands
    
    Added utility methods for interfacing with the befunge interpreter

---
+++ src/utils/BefungeInterpreter.test.ts
@@ -0,0 +1,148 @@
+import { BefungeInterpreter } from "./BefungeInterpreter";
+
+describe("basic programs", () => {
+    test("Hello World", () => {
+        const bf = new BefungeInterpreter([
+            [">", '"', "!", "i", "H", '"', ">", ":", "#", ",", "_", "@"]
+        ]);
+
+        expect(bf.run()).toEqual("Hi!");
+    });
+
+    test("2D plane movement", () => {
+        const bf = new BefungeInterpreter([
+            '>"cb"v> v',
+            'v,"a"<   ',
+            '    @ , <',
+            '>     ^  ',
+        ]);
+
+        expect(bf.run()).toEqual("abc");
+    });
+
+    test("grid wrapping", () => {
+        const bf = new BefungeInterpreter([
+            '>v@   ',
+            'v"<Hi!',
+            ' >v   ',
+            '>:#,_@',
+        ]);
+
+        expect(bf.run()).toEqual("<Hi!vv:>");
+    });
+
+    test("hello world with only one quotation mark", () => {
+        const bf = new BefungeInterpreter([
+            'v       ',
+            '" v  <  ',
+            '>$v  ,  ',
+            '! \\     ',
+            'i :  \\  ',
+            'H > #^_@',
+        ]);
+
+        expect(bf.run()).toEqual("Hi!");
+    });
+
+    test("random direction should choose options w/ equal likelihood", () => {
+        const bf = new BefungeInterpreter([
+            '"dd"*v',
+            'v1<   ',
+            'v2?v#<',
+            'v3< >|',
+            'v4 <:@',
+            '>.1-^ ',
+        ]);
+
+        let res = bf.run();
+        let resVals = res.split(" ").map(n => parseInt(n));
+        let expected = resVals.length / 4;
+        let chisq = 0;
+        for (let i = 1; i <= 4; i++) {
+            let actual = resVals.filter(n => n == i).length;
+            chisq += ((actual - expected)**2) / expected;
+        }
+        expect(chisq).toBeLessThan(11.345); // chisq for 3 DOF, .01 significance level
+            // the idea of this is: we cannot ever prove that the directions are chosen
+            //  equally often, but if we *can* say with 99% confidence that it's unfair,
+            //  then we can be pretty confident that this test is a fail. since this
+            //  test draws 10001 random directions, it will very, very likely detect any
+            //  unfairness
+    });
+
+    test("quine", () => {
+        // quine taken from https://www.codewars.com/kata/526c7b931666d07889000a3c
+        // it's a clever idea: use the get command to literally just grab the code ASCII
+        //  this is done for 45 iterations - the length of the code
+        // i wonder how hard it would be to make a quine without the get command?
+        const bf = new BefungeInterpreter([
+            '01->1# +# :# 0# g# ,# :# 5# 8# *# 4# +# -# _@'
+        ]);
+
+        expect(bf.run()).toEqual('01->1# +# :# 0# g# ,# :# 5# 8# *# 4# +# -# _@');
+    });
+
+    test("numeric inputs, add three numbers", () => {
+        const bf = new BefungeInterpreter([
+            '&&+&+.@'
+        ]);
+
+        bf.feedInput("123 456 789");
+
+        expect(bf.run()).toEqual("1368 ");
+    });
+
+    test("character inputs, cat program", () => {
+        const bf = new BefungeInterpreter([
+            '>"~":v',
+            '     ~',
+            '     \\',
+            '   v-<',
+            '   :  ',
+            '^,+_@ ',
+        ]);
+
+        bf.feedInput("Hi there, this is a cat-like program~~~! It stops at ~.");
+
+        expect(bf.run()).toEqual("Hi there, this is a cat-like program");
+    });
+
+    test("factorial", () => {
+        const bf = new BefungeInterpreter([
+            '195+ v',
+            'vp34:<',
+            '* > :|',
+            '4 -  $',
+            '3 1  .',
+            '>g^  @',
+        ]);
+
+        expect(bf.run()).toEqual("87178291200 ");
+    });
+
+    test("self-modifying calculator", () => {
+        const bf = new BefungeInterpreter([
+            '~60p&& .@'
+        ]);
+
+        const num1 = 8;
+        const num2 = 5;
+        const symbolPairs = [
+            ["+", "13 "],
+            ["-", "3 "],
+            ["*", "40 "],
+            ["/", "1 "],
+            ["%", "3 "],
+            [".", "5 8 "],
+            ["#", ""]
+        ];
+
+        symbolPairs.forEach(pair => {
+            bf.reset();
+            bf.feedInput(pair[0]);
+            bf.feedInput(`${num1} ${num2}`);
+
+            expect(bf.run()).toEqual(pair[1]);
+        });
+    });
+});
--- src/utils/BefungeInterpreter.ts
+++ src/utils/BefungeInterpreter.ts
@@ -1,43 +1,78 @@
+type Dir = "^" | "v" | "<" | ">";
+function isDir(s: string): s is Dir {
+    return "^v<>".includes(s) && s.length == 1;
+}
+
+function isStrArr(s: string[][] | string[]): s is string[] {
+    return (typeof s[0] == "string");
+}
+
 export class BefungeInterpreter {
     #width: number;
     #height: number;
     #initialProgram: string[][];
 
-    #x: number; #vX: number;
-    #y: number; #vY: number;
-    #stringMode: boolean;
-    #program: string[][];
-    #stack: number[];
+    #x: number = 0;
+    #y: number = 0;
+    #dir: Dir = ">";
+    #stringMode: boolean = false;
+    #program: string[][] = [];
+    #stack: number[] = [];
 
-    constructor(program: string[][]) {
+    #input: string = "";
+    #output: string = "";
+    #outputCallback: (s: string) => void = () => {};
+
+    constructor(program: string[][] | string[]) {
+        // getting program dimensions
         this.#height = program.length;
         this.#width = program[0].length;
-        this.#stack = [];
 
-        // copying the given program into an initial state and current state
+        // copying the given program into an initial state
         this.#initialProgram = [];
+        if (isStrArr(program)) {
+            for (let i = 0; i < this.#height; i++) {
+                this.#initialProgram.push(program[i].split(""));
+            }
+        } else {
+            for (let i = 0; i < this.#height; i++) {
+                this.#initialProgram.push([...program[i]]);
+            }
+        }
+
+        // resetting the program state
+        this.reset();
+    }
+
+    reset() {
         this.#program = [];
         for (let i = 0; i < this.#height; i++) {
-            this.#initialProgram.push([...program[i]]);
-            this.#program.push([...program[i]]);
+            this.#program.push([...this.#initialProgram[i]]);
         }
 
         this.#x = 0;
         this.#y = 0;
-        this.#vX = 1;
-        this.#vY = 0;
+        this.#dir = ">";
         this.#stringMode = false;
+
+        this.#stack = [];
+
+        this.#input = "";
+        this.#output = "";
     }
 
-    #push(n: number) {
-        this.#stack.push(n);
-    }
+    setAt(x: number, y: number, value: string, permanent: boolean = false) {
+        if (value.length == 0) {value = " ";}
+        if (value.length > 1) {value = value.substring(1, 2);}
 
-    #pop() {
-        if (this.#stack.length) {
-            return this.#stack.pop();
-        } else {
-            return 0;
+        x = x % this.#width;
+        if (x < 0) {x += this.#width;}
+        y = y % this.#height;
+        if (y < 0) {y += this.#height;}
+
+        this.#program[y][x] = value;
+        if (permanent) {
+            this.#initialProgram[y][x] = value;
         }
     }
 
@@ -53,7 +88,152 @@ export class BefungeInterpreter {
         return [...this.#stack];
     }
 
-    step() {
+    getOutput() {
+        return this.#output;
+    }
 
+    run() {
+        while (this.step()) {}
+
+        return this.#output;
+    }
+
+    step() {
+        // running an instruction
+        const instr = this.#program[this.#y][this.#x];
+
+        if (this.#stringMode) {
+            if (instr == '"') {
+                this.#stringMode = false;
+            } else {
+                this.#push(instr.charCodeAt(0));
+            }
+        } else {
+            const instrCode = instr.charCodeAt(0);
+            if (instrCode >= "0".charCodeAt(0) && instrCode <= "9".charCodeAt(0)) {
+                // number
+                this.#push(parseInt(instr));
+            } else if (isDir(instr)) {
+                // basic movement
+                this.#dir = instr;
+            } else if (instr == "p") {
+                // three-pop instrs (it's just the put instruction)
+                const y = this.#pop();
+                const x = this.#pop();
+                const v = this.#pop();
+                this.setAt(x, y, String.fromCharCode(v));
+            } else if ("+-*/%`\\g".includes(instr)) {
+                // two-pop instrs
+                const a = this.#pop();
+                const b = this.#pop();
+
+                switch (instr) {
+                    case "+": this.#push(a + b); break;
+                    case "-": this.#push(b - a); break;
+                    case "*": this.#push(a * b); break;
+                    case "/": this.#push(a == 0 ? 0 : (b / a)|0); break;
+                    case "%": this.#push(a == 0 ? 0 : b % a); break;
+                    case "`": this.#push(b > a ? 1 : 0); break;
+                    case "\\": this.#push(a); this.#push(b); break;
+                    case "g": this.#push(this.#program[a][b].charCodeAt(0)); break;
+                    default: console.error(`unknown instr ${instr}`);
+                }
+            } else if ("!_|:$.,".includes(instr)) {
+                // one-pop instrs
+                const a = this.#pop();
+
+                switch (instr) {
+                    case "!": this.#push(a == 0 ? 1 : 0); break;
+                    case "_": this.#dir = (a == 0 ? ">" : "<"); break;
+                    case "|": this.#dir = (a == 0 ? "v" : "^"); break;
+                    case ":": this.#push(a); this.#push(a); break;
+                    case "$": break;
+                    case ".": this.#addOutput(a.toString() + " "); break;
+                    case ",": this.#addOutput(String.fromCharCode(a)); break;
+                    default: console.error(`unknown instr ${instr}`);
+                }
+            } else {
+                // other instrs
+
+                switch (instr) {
+                    case "?": this.#dir = "^v<>"[Math.floor(Math.random() * 4)] as Dir; break;
+                    case '"': this.#stringMode = true; break;
+                    case "#": this.#move(); break;
+                    case "&": this.#push(this.#getNumber()); break;
+                    case "~": this.#push(this.#getChar().charCodeAt(0)); break;
+                    case " ": break;
+                    case "@": return false;
+                    default: console.error(`unknown instr ${instr}`);
+                }
+            }
+        }
+
+        // moving the cursor
+        this.#move();
+
+        // returning true to tell the script to keep running
+        return true;
+    }
+
+    feedInput(s: string) {
+        this.#input += s;
+    }
+
+    setOutputCallback(c: (s: string) => void) {
+        this.#outputCallback = c;
+    }
+
+    #push(n: number) {
+        this.#stack.push(n);
+    }
+
+    #pop() {
+        if (this.#stack.length) {
+            return this.#stack.pop() as number;
+        } else {
+            return 0;
+        }
+    }
+
+    #move() {
+        switch (this.#dir) {
+            case "^": this.#y--; break;
+            case "v": this.#y++; break;
+            case "<": this.#x--; break;
+            case ">": this.#x++; break;
+        }
+
+        if (this.#y < 0) {this.#y = this.#height - 1;}
+        if (this.#y >= this.#height) {this.#y = 0;}
+        if (this.#x < 0) {this.#x = this.#width - 1;}
+        if (this.#x >= this.#width) {this.#x = 0;}
+    }
+
+    #getNumber() {
+        while (this.#input.length == 0) {
+            this.feedInput(prompt("number?") ?? "");
+        }
+
+        let spaceInd = this.#input.indexOf(" ");
+        if (spaceInd == -1) {spaceInd = this.#input.length;}
+        const num = parseInt(this.#input.substring(0, spaceInd));
+        this.#input = this.#input.substring(spaceInd + 1);
+
+        return isNaN(num) ? 0 : num;
+    }
+
+    #getChar() {
+        while (this.#input.length == 0) {
+            this.feedInput(prompt("char?") ?? "");
+        }
+
+        const res = this.#input[0];
+        this.#input = this.#input.substring(1);
+        return res;
+    }
+
+    #addOutput(s: string) {
+        this.#output += s;
+        this.#outputCallback(s);
     }
 }
